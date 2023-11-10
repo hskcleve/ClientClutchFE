9ebedfac152333f312f6af36f3c4f38882b85497
Cleve Huang <hskcleve@gmail.com>
2023-11-10 23:38:39 +0800

    change logo adfs

--- client-clutch-fe/src/Navbar.js
+++ client-clutch-fe/src/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./assets/logo.png";
+import adfs from "./assets/adfs.png";
 
 export default function Navbar() {
   return (
@@ -7,7 +7,7 @@ export default function Navbar() {
       className="bg-white flex py-5 px-10 text-white font-semibold gap-10 items-center"
       style={{ backgroundColor: "rgba(25,25,50,100)" }}
     >
-      <img alt="clientclutch logotype" className="h-8" src={logo} />
+      <img alt="clientclutch logotype" className="h-8" src={adfs} />
       <span style={{ color: "rgba(106,106,159,100)" }}>Dashboard</span>
       <span>Live Analysis</span>
     </div>
